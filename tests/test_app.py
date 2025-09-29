154a6126f929bfca2dd9cd5eac8ce79a845b9c57
Jose Carlos Iriarte <josec.iriarte@bg.com.bo>
2025-09-29 12:36:45 -0400

    Modify relative route for testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 import sys, os
 import pytest, json
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app, multiplicar, es_par
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 @pytest.fixture
 def client():
